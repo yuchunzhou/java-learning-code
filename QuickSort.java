ac699208ce24496062cd5e80042b2d3f83992d03
Chunzhou Yu <enjoylife.czzz@gmail.com>
2025-08-02 15:17:05 +0800

    add more code

---
+++ QuickSort.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class QuickSort {
+
+    public int[] sort(int[] sortedNums) {
+        if (sortedNums.length <= 1) {
+            return sortedNums;
+        }
+
+        int pivot = sortedNums[0];
+        ArrayList<Integer> leftArrayList = new ArrayList<Integer>();
+        ArrayList<Integer> rightArrayList = new ArrayList<Integer>();
+
+        for (int i = 1; i < sortedNums.length; i++) {
+            if (sortedNums[i] < pivot) {
+                leftArrayList.add(sortedNums[i]);
+            } else {
+                rightArrayList.add(sortedNums[i]);
+            }
+        }
+
+        int[] result = new int[sortedNums.length];
+
+        int[] leftArray = this.sort(leftArrayList.stream().mapToInt(n -> n).toArray());
+        System.arraycopy(leftArray, 0, result, 0, leftArray.length);
+
+        result[leftArray.length] = pivot;
+
+        int[] rightArray = this.sort(rightArrayList.stream().mapToInt(n -> n).toArray());
+        System.arraycopy(rightArray, 0, result, leftArray.length + 1, rightArray.length);
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] sortedNums = new int[args.length];
+        for (int i = 0; i < args.length; i++) {
+            sortedNums[i] = Integer.parseInt(args[i]);
+        }
+
+        System.out.printf("sorted numbers: %s\n", Arrays.toString(sortedNums));
+
+        QuickSort quickSort = new QuickSort();
+        int[] sortedResult = quickSort.sort(sortedNums);
+
+        System.out.printf("sorted result: %s\n", Arrays.toString(sortedResult));
+    }
+}
